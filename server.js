76fa30bc3de668fad529f81f1462a168aa9dd703
Priscila-Siqueira <priscilasds1997@gmail.com>
2026-03-16 19:30:53 -0300

    atualizado

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
-import express from "express"
-import routes from "./src/routes.js"
+import app from './src/index.js'
+// import conexao from './src/app/database/index.js'
 
-const app = express()
-
-app.use(express.json())
-app.use(routes)
 
 const port = 3000
 
-app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`)
-})
+// //realizando a connection
+// conexao.connect((erro) => {
+//     if (erro) {
+//         console.log(`Falha ao conectar-se ao banco de dados: ${erro}`)
+//     } else {
+//         console.log("Conexão com Banco de Dados realizada com Sucesso")
+//         // Escutando a porta    
+        app.listen(port, () => {
+            console.log(`Servidor rodando http://localhost:${port}`)
+        })
+//     }
+// })
+
 
--- src/app/controllers/SelecaoController.js
+++ src/app/controllers/SelecaoController.js
@@ -1,13 +1,23 @@
-import selecaoRepository from "../repositories/SelecaoRepository.js"
+import conexao from "../database/index.js"
 
 class SelecaoController {
 
-    // listar tudo
+    async executarQuery(sql, valores = []) {
+        try {
+            const [rows] = await conexao.query(sql, valores)
+            return rows
+        } catch (erro) {
+            console.error('Erro na query:', erro)
+            throw erro
+        }
+    }
+
+    //Listar tudo
     async index(req, res) {
+        const sql = "SELECT * FROM dbselecao.dbcopa"
         try {
 
-            const resultado = await selecaoRepository.findAll()
-
+            const resultado = await this.executarQuery(sql)
             res.status(200).json(resultado)
 
         } catch (erro) {
@@ -15,85 +25,72 @@ class SelecaoController {
             res.status(500).json({ erro: "Erro ao buscar seleções" })
 
         }
+
     }
 
-    // buscar por id
+    // Listar por id
     async show(req, res) {
 
         const id = req.params.id
+        const sql = "SELECT * FROM dbselecao.dbcopa WHERE id=?"
 
         try {
-
-            const resultado = await selecaoRepository.findById(id)
-
+            const resultado = await this.executarQuery(sql, [id])
             res.status(200).json(resultado)
-
         } catch (erro) {
-
+            console.log(erro)
             res.status(500).json({ erro: "Erro ao encontrar seleção" })
 
         }
-
     }
 
-    // criar
+    // Criar dados
     async store(req, res) {
-
         const params = req.body
+        const sql = "INSERT INTO dbselecao.dbcopa SET ?;"
 
         try {
-
-            const resultado = await selecaoRepository.create(params)
-
+            const resultado = await this.executarQuery(sql, [params])
             res.status(201).json(resultado)
-
         } catch (erro) {
-
+            console.log(erro)
             res.status(500).json({ erro: "Erro ao inserir seleção" })
-
         }
+
 
     }
 
-    // atualizar
-    async update(req, res) {
 
+    // Atualizar dados
+    async update(req, res) {
         const id = req.params.id
         const params = req.body
-
+        const sql = "UPDATE dbselecao.dbcopa SET ? WHERE id = ?;"
         try {
-
-            const resultado = await selecaoRepository.update(id, params)
-
-            res.status(200).json(resultado)
-
+            const resultado = await this.executarQuery(sql, [params, id])
+            res.status(201).json(resultado)
         } catch (erro) {
-
+            console.log(erro)
             res.status(500).json({ erro: "Erro ao atualizar seleção" })
-
         }
+
+
 
     }
 
-    // deletar
+    // Deletar dados
     async delete(req, res) {
-
         const id = req.params.id
-
+        const sql = "DELETE FROM dbselecao.dbcopa WHERE id=?"
         try {
-
-            const resultado = await selecaoRepository.delete(id)
-
-            res.status(200).json(resultado)
-
+            const resultado = await this.executarQuery(sql, [id])
+            res.status(201).json(resultado)
         } catch (erro) {
-
+            console.log(erro)
             res.status(500).json({ erro: "Erro ao deletar seleção" })
-
         }
-
     }
 
 }
-
+// Padrão singleton
 export default new SelecaoController()
